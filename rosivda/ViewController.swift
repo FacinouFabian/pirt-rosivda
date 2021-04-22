c19d8a421284f570e3192ea5cf172f3fa30258ce
FacinouFabian <fabianfacinou02@gmail.com>
2021-04-22 00:02:47 -0700

    first commit

--- rosivda/ViewController.swift
+++ rosivda/ViewController.swift
@@ -6,12 +6,48 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        if CLLocationManager.locationServicesEnabled() {
+           // continue to implement here
+            checkLocationAuthorization()
+        } else {
+           // Do something to let users know why they need to turn it on.
+        }
+    }
+    
+    func checkLocationAuthorization() {
+      switch locationManager.authorizationStatus {
+      case .authorizedWhenInUse:
+        mapView.showsUserLocation = true
+       case .denied: // Show alert telling users how to turn on permissions
+       break
+      case .notDetermined:
+        locationManager.requestWhenInUseAuthorization()
+        mapView?.showsUserLocation = true
+      case .restricted: // Show an alert letting them know what’s up
+       break
+      case .authorizedAlways:
+       break
+      @unknown default:
+        break
+      }
+    }
+    
+    func checkLocationServices() {
+      if CLLocationManager.locationServicesEnabled() {
+        checkLocationAuthorization()
+      } else {
+        // Show alert letting the user know they have to turn this on.
+      }
     }
 
 
