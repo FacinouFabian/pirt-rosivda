35c7974a38f01cb59d2dcb2bfb2a504d450d9a99
FacinouFabian <fabianfacinou02@gmail.com>
2021-04-21 23:22:21 -0700

    Initial Commit

---
+++ rosivda/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rosivda
+//
+//  Created by Fabian on 21/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
